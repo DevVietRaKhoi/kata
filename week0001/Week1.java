0c976b875f0a77cb725af8240e9a003ae5b535e3
vietthaotran <tranphamvietthao@gmail.com>
2016-09-27 20:06:21 +0800

    Update Week1.java

--- week0001/Week1.java
+++ week0001/Week1.java
@@ -1,7 +1,6 @@
 public class Week1 {
     public static void main(String[] args) {
         for(int i=1; i<101; i++){
-            System.out.println("Count is: " + i);
             if (i % 3 == 0 ){
 	            System.out.println("Fizz");
             }
