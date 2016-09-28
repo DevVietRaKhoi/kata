c7d58125952f94679954691fe9190746b2c00363
vietthaotran <tranphamvietthao@gmail.com>
2016-09-28 20:29:22 +0800

    Update and rename Week1.java to FizzBuzzThao.java

--- week0001/FizzBuzzThao.java
+++ week0001/FizzBuzzThao.java
@@ -1,10 +1,10 @@
 public class Week1 {
     public static void main(String[] args) {
-        for(int i=1; i<101; i++){
-            if (i % 3 == 0 ){
+        for(int i = 1; i < 101; i++) {
+            if (i % 3 == 0 ) {
 	            System.out.println("Fizz");
             }
-            if (i % 5 == 0 ){
+            if (i % 5 == 0 ) {
 	            System.out.println("Buzz");
             }
         }
