2cee11a7f36a9af4bfb87ea04e3252790bb53116
hienhpss <thanhhien008@gmail.com>
2016-09-30 01:45:56 +0800

    Python generators: Solution for fizzbuzz (#11)
    * Solution for week1 fizzbuzz's problem
    
    * Fizzbuzz solution by Nam
    
    * Fixing duplicates
    
    * Fixing whitespaces

---
+++ week0001/fizzbuzz_nam.py
@@ -0,0 +1,14 @@
+def fizzbuzz(n=100):
+	loop = (x % 15 for x in range(1,n+1) if x % 3 == 0 or x % 5 == 0)
+	for x in loop:
+		if x in (3,6,9,12):
+			yield "Fizz"
+		elif x in (5,10):
+			yield "Buzz"
+		else:
+			yield "Fizz\nBuzz"
+
+if __name__ == "__main__":
+	for x in fizzbuzz(100):
+		print(x)
+	
