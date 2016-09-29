4c8b9417fc74f15aa5fdb295d23ab8e47cf5d577
namhi89 <namtt.7@gmail.com>
2016-09-29 13:34:59 -0400

    Fixing duplicates

--- week0001/fizzbuzz_nam.py
+++ week0001/fizzbuzz_nam.py
@@ -2,11 +2,11 @@ def fizzbuzz(n=100):
 	loop = (x%15 for x in range(1,n+1) if x%3==0 or x%5==0)
 	for x in loop:
 		if x in (3,6,9,12):
-			yield "fizz"
+			yield "Fizz"
 		elif x in (5,10):
-			yield "buzz"
+			yield "Buzz"
 		else:
-			yield "fizz\nbuzz"
+			yield "Fizz\nBuzz"
 
 if __name__ == "__main__":
 	for x in fizzbuzz(100):
