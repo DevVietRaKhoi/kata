d7b3be1c82c6fb03fef7807c242c0078fe6e872a
namhi89 <namtt.7@gmail.com>
2016-09-29 13:40:48 -0400

    Fixing whitespaces

--- week0001/fizzbuzz_nam.py
+++ week0001/fizzbuzz_nam.py
@@ -1,5 +1,5 @@
 def fizzbuzz(n=100):
-	loop = (x%15 for x in range(1,n+1) if x%3==0 or x%5==0)
+	loop = (x % 15 for x in range(1,n+1) if x % 3 == 0 or x % 5 == 0)
 	for x in loop:
 		if x in (3,6,9,12):
 			yield "Fizz"
