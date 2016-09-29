031c8169d80e3eca34a2ed7f27644999dfacc22c
Nguyen Duy Hao <nguyenduyhao1111@gmail.com>
2016-09-29 20:36:32 +0700

    add space

--- week0001/fizz_buzz_haond.rb
+++ week0001/fizz_buzz_haond.rb
@@ -2,11 +2,11 @@ def fizz_buzz(n)
   x = 0
   y = 0
   while(x + 3 <= n || y + 5 <= n)
-    if(x+3<=y+5)
-        x+=3
+    if(x + 3 <= y + 5)
+        x += 3
         puts "Fizz"
     else
-        y+=5
+        y += 5
         puts "Buzz"
     end
   end
