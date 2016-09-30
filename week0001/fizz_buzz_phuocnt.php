fbf40a6bcd7e8dff9dda489e3416376a6acf036f
at-phuocnt <phuocnt1@asiantech.vn>
2016-09-30 16:41:31 +0700

    Fix grammar mistake

--- week0001/fizz_buzz_phuocnt.php
+++ week0001/fizz_buzz_phuocnt.php
@@ -3,12 +3,12 @@
 if (! function_exists('printFizzBuzz')) {
     function printFizzBuzz($begin, $end, $requirementData) {
         if ($begin > $end) {
-            throw new Exception('Begin number cannot greater than end number');
+            throw new Exception('Begin number cannot be greater than end number');
         }
         for ($i = $begin; $i <= $end; $i++) {
             foreach ($requirementData as $numberDivided => $stringEcho) {
                 if ($i % $numberDivided == 0) {
-                    echo $stringEcho . '</br>';
+                    echo $stringEcho . '<br/>';
                 }
             }
         }
