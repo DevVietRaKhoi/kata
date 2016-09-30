45facac4e03c702e4cb379f989fdc80540248f56
Michal Plachy <michal.plachy@framgia.com>
2016-09-30 08:28:13 +0700

    fix golang and php

--- week0001/FizzBuzzMichal.php
+++ week0001/FizzBuzzMichal.php
@@ -2,6 +2,8 @@
 
 for($counter = 1; $counter <= 100; $counter++)
 {
-    echo 0 == $counter%3 ? 'Fizz<br>' : '';
-    echo 0 == $counter%5 ? 'Buzz<br>' : '';
+    if (0 == $counter % 3)
+        echo 'Fizz<br>';
+    if (0 == $counter % 5)
+        echo 'Buzz<br>';
 }
--- week0001/FizzBuzzMichalGo.go
+++ week0001/FizzBuzzMichalGo.go
@@ -5,11 +5,11 @@ import (
 )
 
 func main() {
-	for i := 1; i < 100; i++ {
-		if 0 == i%3 {
+	for i := 1; i <= 100; i++ {
+		if 0 == i % 3 {
 			fmt.Println("Fizz")
 		}
-		if 0 == i%5 {
+		if 0 == i % 5 {
 			fmt.Println("Buzz")
 		}
 	}
