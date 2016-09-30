aba6197d6cae268b69e83c7357f02e53007cdfa3
Phuoc Nguyen Thanh <phuocnt1@asiantech.vn>
2016-09-30 16:44:53 +0700

    PHP solution: week1_fizz_buzz_phuocnt (#13)
    * week1_fizz_buzz_phuocnt
    
    * Fix grammar mistake

---
+++ week0001/fizz_buzz_phuocnt.php
@@ -0,0 +1,22 @@
+<?php
+
+if (! function_exists('printFizzBuzz')) {
+    function printFizzBuzz($begin, $end, $requirementData) {
+        if ($begin > $end) {
+            throw new Exception('Begin number cannot be greater than end number');
+        }
+        for ($i = $begin; $i <= $end; $i++) {
+            foreach ($requirementData as $numberDivided => $stringEcho) {
+                if ($i % $numberDivided == 0) {
+                    echo $stringEcho . '<br/>';
+                }
+            }
+        }
+    }
+}
+
+$requirementData = [
+    '3' => 'Fizz',
+    '5' => 'Buzz'
+];
+printFizzBuzz(1, 100, $requirementData);
