c7a0d7812481dde7e579b4c2a622015a9b0bb726
namhi89 <namtt.7@gmail.com>
2016-09-30 00:40:17 -0400

    Adding straight forward solution

--- week0001/fizzbuzz_nam.py
+++ week0001/fizzbuzz_nam.py
@@ -1,4 +1,6 @@
 def fizzbuzz(n=100):
+'''Using generator to separate between
+generating and using data'''
 	loop = (x % 15 for x in range(1,n+1) if x % 3 == 0 or x % 5 == 0)
 	for x in loop:
 		if x in (3,6,9,12):
@@ -7,6 +9,14 @@ def fizzbuzz(n=100):
 			yield "Buzz"
 		else:
 			yield "Fizz\nBuzz"
+
+def fizzbuzz2(n=100):
+'''Straight forward solution'''
+	for i in range(1,n+1):
+		if i % 3 == 0:
+			print("Fizz")
+		if i % 5 == 0:
+			print("Buzz")
 
 if __name__ == "__main__":
 	for x in fizzbuzz(100):
