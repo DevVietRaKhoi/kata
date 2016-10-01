ae5ae883acce7aef7300f1f777277b80d8ad747a
bangnokia <bangnokia@gmail.com>
2016-10-02 02:24:08 +0700

    update

--- week0001/fizzbuzz_daudau.php
+++ week0001/fizzbuzz_daudau.php
@@ -1,8 +1,8 @@
 <?php
 	$i = 1;
-	$fizz = 'fizz'.'<br/>'; $buzz = 'buzz'.'<br/>';
+	$fizz = 'fizz'.'<br/>';
+	$buzz = 'buzz'.'<br/>';
 	$a = 3; $b =5;
 	$ab = $a*$b;
-	while ($i++ < 100){
+	while ($i++ < 100)
 		echo $i%$ab ? $i%$a ? $i%$b ? false : $buzz : $fizz : $fizz.$buzz;
-	}
