29ccdc0e0b842bd7633d14c4201130c5f9b608b5
bangnokia <bangnokia@gmail.com>
2016-10-02 02:22:14 +0700

    udpate to more  understandable

--- week0001/fizzbuzz_daudau.php
+++ week0001/fizzbuzz_daudau.php
@@ -1,4 +1,8 @@
 <?php
 	$i = 1;
-	while ($i++ < 100)
-		echo $i%15 ? $i%3 ? $i%5 ? '' : 'buzz'.'<br/>' : 'fizz'.'<br/>' : 'fizz'.'<br/>'.'buzz'.'<br/>';
+	$fizz = 'fizz'.'<br/>'; $buzz = 'buzz'.'<br/>';
+	$a = 3; $b =5;
+	$ab = $a*$b;
+	while ($i++ < 100){
+		echo $i%$ab ? $i%$a ? $i%$b ? false : $buzz : $fizz : $fizz.$buzz;
+	}
