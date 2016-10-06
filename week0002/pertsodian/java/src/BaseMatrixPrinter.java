ff4b0bb052e51d2734b1d937b92b02399f9f5911
Harry Tran <ttphu.harry@gmail.com>
2016-10-07 01:45:51 +0800

    Adjust whitespace character

--- week0002/pertsodian/java/src/BaseMatrixPrinter.java
+++ week0002/pertsodian/java/src/BaseMatrixPrinter.java
@@ -2,29 +2,29 @@ import java.util.Arrays;
 import java.util.StringJoiner;
 
 public abstract class BaseMatrixPrinter {
-	
-	private String delimiter = " ";
-	private String lineSeparator = "\n";
-	
-	public abstract void printMatrix(int[][] matrix);
-	
-	protected final void setDelimiter(String delimiter) {
-		this.delimiter = delimiter;
-	}
 
-	protected final void setLineSeparator(String lineSeparator) {
-		this.lineSeparator = lineSeparator;
-	}
+  private String delimiter = " ";
+  private String lineSeparator = "\n";
 
-	protected final String print(int[][] matrix) {
-		StringJoiner lineJoiner = new StringJoiner(lineSeparator);
-		Arrays.stream(matrix).forEach(row -> lineJoiner.add(printRow(row)));
-		return lineJoiner.toString();
-	}
-	
-	private String printRow(int[] row) {
-		StringJoiner elementJoiner = new StringJoiner(delimiter);
-		Arrays.stream(row).forEach(element -> elementJoiner.add(String.valueOf(element)));
-		return elementJoiner.toString();
-	}
+  public abstract void printMatrix(int[][] matrix);
+
+  protected final void setDelimiter(String delimiter) {
+    this.delimiter = delimiter;
+  }
+
+  protected final void setLineSeparator(String lineSeparator) {
+    this.lineSeparator = lineSeparator;
+  }
+
+  protected final String print(int[][] matrix) {
+    StringJoiner lineJoiner = new StringJoiner(lineSeparator);
+    Arrays.stream(matrix).forEach(row -> lineJoiner.add(printRow(row)));
+    return lineJoiner.toString();
+  }
+
+  private String printRow(int[] row) {
+    StringJoiner elementJoiner = new StringJoiner(delimiter);
+    Arrays.stream(row).forEach(element -> elementJoiner.add(String.valueOf(element)));
+    return elementJoiner.toString();
+  }
 }
--- week0002/pertsodian/java/src/ConsoleMatrixPrinter.java
+++ week0002/pertsodian/java/src/ConsoleMatrixPrinter.java
@@ -1,6 +1,6 @@
 public class ConsoleMatrixPrinter extends BaseMatrixPrinter {
-	@Override
-	public void printMatrix(int[][] matrix) {
-		System.out.println(print(matrix));
-	}
+  @Override
+  public void printMatrix(int[][] matrix) {
+    System.out.println(print(matrix));
+  }
 }
