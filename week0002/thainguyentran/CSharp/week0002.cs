1ca21962661103154615d16ef8b12fcc02475e49
thainguyentran <thainguyentran@icloud.com>
2016-10-07 14:54:40 +0700

    fix minor errors
    collum => column
    fix displaying for number larger than 10

--- week0002/thainguyentran/CSharp/week0002.cs
+++ week0002/thainguyentran/CSharp/week0002.cs
@@ -14,7 +14,7 @@ namespace DVRK
             int[,] spiral = new int[N, M];
             int value = 1;
             int posX = N - 1, posY = M - 1;
-            int row = 0, collum = 0;
+            int row = 0, column = 0;
 
             while (value < N * M)
             {
@@ -24,32 +24,31 @@ namespace DVRK
                     spiral[row, i] = value++;
                 }
                 //going down
-                for (int j = collum + 1; j <= posX; j++)
+                for (int j = column + 1; j <= posX; j++)
                 {
                     spiral[j, posY] = value++;
                 }
                 //going left
-                for (int i = posY - 1; i >= collum; i--)
+                for (int i = posY - 1; i >= column; i--)
                 {
                     spiral[posX, i] = value++;
                 }
                 //going up
                 for (int j = posX - 1; j >= row + 1; j--)
                 {
-                    spiral[j, collum] = value++;
+                    spiral[j, column] = value++;
                 }
                 row++;
-                collum++;
+                column++;
                 posX--;
                 posY--;
             }
+            int maxLen = (N * M).ToString().Length;
             for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < M; j++)
                 {
-                    if (spiral[i, j] < 10)
-                        Console.Write("  {0}", spiral[i, j]);
-                    else Console.Write(" {0}", spiral[i, j]);
+                    Console.Write("{0}", spiral[i, j].ToString().PadLeft(maxLen + 1,' '));
                 }
                 Console.Write("\n");
             }
