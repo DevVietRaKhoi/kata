28106308e12d29f69343b5c3dae70b343e875d9f
ngo dang truong <truongngodang@gmail.com>
2016-10-07 20:04:28 +0700

    Removed code unuse

--- week0002/truongngodang/java/week0002.java
+++ week0002/truongngodang/java/week0002.java
@@ -6,42 +6,8 @@ public class week0002 {
 
     public static void main(String[] args) {
         int[][] array = new int[4][4];
-//        createSpiralSquare(array);
         createSprialRectangle(array, 1);// 1: start form 1;
         displayArrayToConsole(array);
-    }
-
-    private static void createSpiralSquare(int[][] array) {
-        int row = array.length - 1;
-        int col = array.length - 1;
-        int value = 1;
-        int flag = 0;
-
-        while (flag <= array.length / 2) {
-            // Draw top row
-            for (int colTop = flag; colTop <= col; colTop++) {
-                array[flag][colTop] = value;
-                value++;
-            }
-            // Draw right col
-            for (int rowRight = flag + 1; rowRight <= row; rowRight++) {
-                array[rowRight][col] = value;
-                value++;
-            }
-            // Draw bottom row
-            for (int colBottom = col - 1; colBottom >= flag; colBottom--) {
-                array[row][colBottom] = value;
-                value++;
-            }
-            // Draw left col
-            for (int rowLeft = row - 1; rowLeft >= flag + 1; rowLeft--) {
-                array[rowLeft][flag] = value;
-                value++;
-            }
-            flag++;
-            row--;
-            col--;
-        }
     }
 
     private static void createSprialRectangle(int[][] array, int numStart) {
