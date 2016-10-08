954b412ac40e9ec076190c155b822c0a055c4a12
namhi89 <namtt.7@gmail.com>
2016-10-08 05:56:47 -0400

    Fixing spacing

--- week0002/hienhpss/python/week0002.py
+++ week0002/hienhpss/python/week0002.py
@@ -3,13 +3,13 @@ def spi_round(n=4, m=4, x=0):
 	cell'''
 	if (x >= (n+1) / 2) or (x >= (m+1) / 2):
 		return
-	for i in range(x, m-x-1):
+	for i in range(x, m - x -1):
 		yield (x, i)
-	for i in range(x, n-x-1):
-		yield(i, m-x-1)
-	for i in range(m-x-1, x,-1):
-		yield(n-x-1, i)
-	for i in range(n-x-1, x,-1):
+	for i in range(x, n - x - 1):
+		yield(i, m - x - 1)
+	for i in range(m - x - 1, x, -1):
+		yield(n - x - 1, i)
+	for i in range(n - x - 1, x, -1):
 		yield(i, x)
 
 def spiral(n=4, m=4):
@@ -20,7 +20,7 @@ def spiral(n=4, m=4):
 	count = 1
 	for i in range(limit):
 		for next_row, next_col in spi_round(n, m, i):
-			if board[next_row][next_col] == False:
+			if not board[next_row][next_col]:
 				board[next_row][next_col] = count
 				count += 1
 			else:
@@ -29,7 +29,7 @@ def spiral(n=4, m=4):
 
 def test_spiral(n, m):
 	result = spiral(n, m)
-	print('\n'.join([' '.join(['{:4}'.format(item) for item in row]) 
+	print('\n'.join([' '.join(['{:2}'.format(item) for item in row]) 
       for row in result]))
 
 if __name__=="__main__":
