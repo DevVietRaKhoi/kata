e3a0ea0b0eb4aefe3822816e08bb7596649b3a90
nhantd <trannhan1203@gmail.com>
2016-10-12 22:29:17 +0100

    fixing minor bugs

--- week0003/nhantd/python/week0003.py
+++ week0003/nhantd/python/week0003.py
@@ -1,41 +1,42 @@
 import sys
 
 
-class Users():
+class Accounts():
 
     def __init__(self):
         self.data = dict()
 
     def addAccount(self, Name, Amount):
-        if Name in self.data:
-            return 'False'
-        else:
+        if Name not in self.data and Amount >= 0:
             self.data[Name] = Amount
             return 'True'
+        else:
+            return 'False'
 
     def depositMoney(self, Name, Amount):
-        if Name not in self.data:
-            return 'False'
-        else:
+        if Name in self.data and Amount >= 0:
             self.data[Name] = self.data[Name] + Amount
             return 'True'
+        else:
+            return 'False'
 
     def withdrawMoney(self, Name, Amount):
-        if Name in self.data and Amount <= self.data[Name]:
+        if Name in self.data and Amount >= 0 and Amount <= self.data[Name]:
             self.data[Name] = self.data[Name] - Amount
             return 'True'
         else:
             return 'False'
 
-sampleData = Users()
-for line in sys.stdin:
-    Key, Name, Amount = line.strip().split(' ')
-    Amount = float(Amount)
-    if Key == 'A':
-        print sampleData.addAccount(Name, Amount)
-    elif Key == 'D':
-        print sampleData.depositMoney(Name, Amount)
-    elif Key == 'W':
-        print sampleData.withdrawMoney(Name, Amount)
-    else:
-        print 'Invalid'
+if __name__ == "__main__":
+    sampleAccounts = Accounts()
+    for line in sys.stdin:
+        Key, Name, Amount = line.strip().split(' ')
+        Amount = float(Amount)
+        if Key == 'A':
+            print sampleAccounts.addAccount(Name, Amount)
+        elif Key == 'D':
+            print sampleAccounts.depositMoney(Name, Amount)
+        elif Key == 'W':
+            print sampleAccounts.withdrawMoney(Name, Amount)
+        else:
+            print 'Invalid'
