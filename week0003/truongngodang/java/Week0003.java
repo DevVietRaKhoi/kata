d370893fd173ca56d0f4746feb0514776058030d
ngo dang truong <truongngodang@gmail.com>
2016-10-12 20:54:24 +0700

    refactor code

--- week0003/truongngodang/java/Week0003.java
+++ week0003/truongngodang/java/Week0003.java
@@ -1,46 +1,49 @@
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Week0003 {
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        String[] stringRequest = new String[12];
-        ProcessRequest[] processRequests = new ProcessRequest[12];
-        ArrayList<Client> clients = new ArrayList<Client>();
+        ArrayList<String> stringRequest = new ArrayList<String>();
+        ArrayList<ProcessStringRequest> processRequests = new ArrayList<ProcessStringRequest>();
+        HashMap<String, Client> clients = new HashMap<String, Client>();
         ArrayList<String> output = new ArrayList<String>();
 
-        for (int i = 0; i < stringRequest.length; i++) {
-            stringRequest[i] = scan.nextLine();
-            processRequests[i] = new ProcessRequest(stringRequest[i]);
+        for (int i = 0; scan.hasNext(); i++) {
+            stringRequest.add(scan.nextLine());
+            processRequests.add(new ProcessStringRequest(stringRequest.get(i)));
         }
+        scan.close();
 
-        for (ProcessRequest value: processRequests) {
+        processRequest(processRequests, clients, output);
+
+        output.forEach(System.out::println);
+    }
+
+    static void processRequest(ArrayList<ProcessStringRequest> processRequests,
+                               HashMap<String, Client> clients,
+                               ArrayList<String> output) {
+        for (ProcessStringRequest value : processRequests) {
             if (Objects.equals(value.getDefineRequest(), "A")) {
-                if (Client.findClientByName(clients, value.getName()) == null) {
-                    clients.add(new Client(value.getName(), value.getAmount()));
+                if (clients.get(value.getName()) == null) {
+                    clients.put(value.getName(), new Client(value.getName(), value.getAmount()));
                     output.add("True");
                 } else output.add("False");
             } else if (Objects.equals(value.getDefineRequest(), "D")) {
-                if (Client.findClientByName(clients, value.getName()) != null) {
-                    Client.findClientByName(clients, value.getName()).deposit(value.getAmount());
+                if (clients.get(value.getName()) != null) {
+                    clients.get(value.getName()).deposit(value.getAmount());
                     output.add("True");
                 } else output.add("False");
             } else if (Objects.equals(value.getDefineRequest(), "W")) {
-                if (Client.findClientByName(clients, value.getName()) != null
-                        && Client.findClientByName(clients, value.getName()).getAmount() > value.getAmount()) {
-                    Client.findClientByName(clients, value.getName()).wthdraw(value.getAmount());
+                if (clients.get(value.getName()) != null
+                        && clients.get(value.getName()).getAmount() > value.getAmount()) {
+                    clients.get(value.getName()).withdraw(value.getAmount());
                     output.add("True");
                 } else output.add("False");
             } else {
                 output.add("Invalid");
             }
-        }
-
-        for (String value:output) {
-            System.out.println(value);
         }
     }
 }
@@ -71,35 +74,28 @@ class Client {
         this.amount = amount;
     }
 
-    static Client findClientByName(ArrayList<Client> clients, String name) {
-        for (Client client : clients) {
-            if (Objects.equals(client.getName(), name)) {
-                return client;
-            }
-        }
-        return null;
-    }
     void deposit(Double amount) {
         this.amount += amount;
     }
-    void wthdraw(Double amount) {
+
+    void withdraw(Double amount) {
         this.amount -= amount;
     }
 }
 
-class ProcessRequest {
-    
+class ProcessStringRequest {
+
     private String defineRequest;
     private String name;
     private double amount;
 
-    public ProcessRequest(String defineRequest, String name, double amount) {
+    public ProcessStringRequest(String defineRequest, String name, double amount) {
         this.defineRequest = defineRequest;
         this.name = name;
         this.amount = amount;
     }
 
-    public ProcessRequest(String stringRequest) {
+    public ProcessStringRequest(String stringRequest) {
         String[] value = stringRequest.split(" ");
         this.defineRequest = value[0];
         this.name = value[1];
