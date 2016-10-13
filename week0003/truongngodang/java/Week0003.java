fc4381f9561a66812f939f38457b30a08c75e4d3
ngo dang truong <truongngodang@gmail.com>
2016-10-13 21:41:28 +0700

    Refactor Code 3st

--- week0003/truongngodang/java/Week0003.java
+++ week0003/truongngodang/java/Week0003.java
@@ -1,101 +1,80 @@
 import java.util.*;
 
 public class Week0003 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        HashMap<String, Client> clientHashMap = new HashMap<String, Client>();
+        ArrayList<String> output = new ArrayList<String>();
+        ClientManagement clientManagement = new ClientManagement(clientHashMap);
+        while (scan.hasNext()) {
+            output.add(clientManagement.processRequest(scan.nextLine()));
+        }
+        for (int i = 0; i < output.size(); i++) {
+            System.out.println(output.get(i));
+        }
+    }
+}
+
+class ClientManagement {
 
     private static final String ADD_NEW_CLIENT = "A";
     private static final String DEPOSIT_MONEY = "D";
     private static final String WITHDRAW_MONEY = "W";
 
-    public static void main(String[] args) {
+    private HashMap<String, Client> clientHashMap;
 
-        Scanner scan = new Scanner(System.in);
-        HashMap<String, Client> clients = new HashMap<String, Client>();
-        ArrayList<String> output = new ArrayList<String>();
-
-        while (scan.hasNext()) {
-            processRequest(new ProcessStringRequest(scan.nextLine()), clients, output);
-        }
-        scan.close();
-        for (int i = 0; i < output.size(); i++) {
-            System.out.println(output.get(i));
-        }
+    public ClientManagement(HashMap<String, Client> clientHashMap) {
+        this.clientHashMap = clientHashMap;
     }
 
-    private static void processRequest(ProcessStringRequest processStringRequest,
-                                       HashMap<String, Client> clients,
-                                       ArrayList<String> output) {
+    public boolean addNewClient(String name, Double money) {
+        if (this.clientHashMap.get(name) == null) {
+            this.clientHashMap.put(name, new Client(name, money));
+            return true;
+        }
+        return false;
+    }
 
-            switch (processStringRequest.getDefineRequest()) {
-                case ADD_NEW_CLIENT: {
-                    addNewClient(processStringRequest, clients, output);
-                    break;
+    public String processRequest(String request) {
+        ProcessString processString = new ProcessString(request);
+        switch (processString.getDefineRequest()) {
+            case ADD_NEW_CLIENT : {
+                if (addNewClient(processString.getName(), processString.getMoney())) {
+                    return "True";
+                } else {
+                    return "False";
                 }
-                case DEPOSIT_MONEY: {
-                    depositMoney(processStringRequest, clients, output);
-                    break;
-                }
-                case WITHDRAW_MONEY: {
-                    withdrawMoney(processStringRequest, clients, output);
-                    break;
-                }
-                default:
-                    output.add("Invalid");
-
             }
-    }
-
-    private static void addNewClient(ProcessStringRequest processStringRequest,
-                                     HashMap<String, Client> clients,
-                                     ArrayList<String> output) {
-        if (clients.get(processStringRequest.getName()) == null
-                && processStringRequest.getAmount() > 0) {
-
-            clients.put(processStringRequest.getName(),
-                    new Client(processStringRequest.getName(),
-                            processStringRequest.getAmount()));
-            output.add("True");
-        }
-        else {
-            output.add("False");
-        }
-    }
-
-    private static void depositMoney(ProcessStringRequest processStringRequest,
-                                     HashMap<String, Client> clients,
-                                     ArrayList<String> output) {
-        if (clients.get(processStringRequest.getName()) != null
-                && processStringRequest.getAmount() > 0) {
-
-            clients.get(processStringRequest.getName()).deposit(processStringRequest.getAmount());
-            output.add("True");
-        }
-        else {
-            output.add("False");
-        }
-    }
-    
-    private static void withdrawMoney(ProcessStringRequest processStringRequest,
-                                      HashMap<String, Client> clients,
-                                      ArrayList<String> output) {
-        if (clients.get(processStringRequest.getName()) != null
-                && processStringRequest.getAmount() > 0
-                && clients.get(processStringRequest.getName()).getAmount() > processStringRequest.getAmount()) {
-
-            clients.get(processStringRequest.getName()).withdraw(processStringRequest.getAmount());
-            output.add("True");
-        }
-        else {
-            output.add("False");
+            case DEPOSIT_MONEY : {
+                if (this.clientHashMap.get(processString.getName()) != null) {
+                    this.clientHashMap.get(processString.getName()).depositMoney(processString.getMoney());
+                    return "True";
+                } else {
+                    return "False";
+                }
+            }
+            case WITHDRAW_MONEY : {
+                if (this.clientHashMap.get(processString.getName()) != null) {
+                    this.clientHashMap.get(processString.getName()).withdrawMoney(processString.getMoney());
+                    return "True";
+                } else {
+                    return "False";
+                }
+            }
+            default:
+                return "Invalid";
         }
     }
 }
 
 class Client {
-
     private String name;
     private Double amount;
 
-    public Client(String name, double amount) {
+    public Client() {
+    }
+
+    public Client(String name, Double amount) {
         this.name = name;
         this.amount = amount;
     }
@@ -108,40 +87,47 @@ class Client {
         this.name = name;
     }
 
-    public double getAmount() {
+    public Double getAmount() {
         return amount;
     }
 
-    public void setAmount(double amount) {
+    public void setAmount(Double amount) {
         this.amount = amount;
     }
 
-    void deposit(Double amount) {
-        this.amount += amount;
+    public boolean depositMoney(Double money) {
+        if (money > 0) {
+            this.amount += money;
+            return true;
+        }
+        return false;
     }
 
-    void withdraw(Double amount) {
-        this.amount -= amount;
+    public boolean withdrawMoney(Double money) {
+        if (money > 0 && this.amount >= money) {
+            this.amount -= money;
+            return true;
+        }
+        return false;
     }
 }
 
-class ProcessStringRequest {
-
+class ProcessString {
     private String defineRequest;
     private String name;
-    private double amount;
+    private double money;
 
-    public ProcessStringRequest(String defineRequest, String name, double amount) {
+    public ProcessString(String defineRequest, String name, double amount) {
         this.defineRequest = defineRequest;
         this.name = name;
-        this.amount = amount;
+        this.money = amount;
     }
 
-    public ProcessStringRequest(String stringRequest) {
-        String[] value = stringRequest.split(" ");
+    public ProcessString(String string) {
+        String[] value = string.split(" ");
         this.defineRequest = value[0];
         this.name = value[1];
-        this.amount = Double.valueOf(value[2]);
+        this.money = Double.valueOf(value[2]);
     }
 
     public String getDefineRequest() {
@@ -160,11 +146,11 @@ class ProcessStringRequest {
         this.name = name;
     }
 
-    public double getAmount() {
-        return amount;
+    public double getMoney() {
+        return money;
     }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
+    public void setMoney(double amount) {
+        this.money = amount;
     }
 }
