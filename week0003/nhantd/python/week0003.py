69c6799f762e902da95b9287da1fa427ecbadd13
nhantd <trannhan1203@gmail.com>
2016-10-13 21:25:56 +0100

    refactor code & fixing minor bugs

--- week0003/nhantd/python/week0003.py
+++ week0003/nhantd/python/week0003.py
@@ -1,5 +1,9 @@
 import sys
 
+ACTION_ADD_ACCOUNT = 'A'
+ACTION_DEPOSIT_MONEY = 'D'
+ACTION_WITHDRAW_MONEY = 'W'
+
 
 class Accounts():
 
@@ -7,40 +11,40 @@ class Accounts():
         self.data = dict()
 
     def addAccount(self, name, amount):
-        if name not in self.data and amount >= 0:
+        if name not in self.data and amount > 0:
             self.data[name] = amount
             return 'True'
         else:
             return 'False'
 
     def depositMoney(self, name, amount):
-        if name in self.data and amount >= 0:
+        if name in self.data and amount > 0:
             self.data[name] = self.data[name] + amount
             return 'True'
         else:
             return 'False'
 
     def withdrawMoney(self, name, amount):
-        if name in self.data and amount >= 0 and amount <= self.data[name]:
+        if name in self.data and amount > 0 and amount <= self.data[name]:
             self.data[name] = self.data[name] - amount
             return 'True'
         else:
             return 'False'
 
-    def executeTransaction(self, inputTransaction):
-        key, name, amount = inputTransaction.strip().split(' ')
-        amount = float(amount)
-        if key == 'A':
-            return self.addAccount(name, amount)
-        elif key == 'D':
-            return self.depositMoney(name, amount)
-        elif key == 'W':
-            return self.withdrawMoney(name, amount)
-        else:
-            return 'Invalid'
 
+def executeTransaction(accounts, inputTransaction):
+    key, name, amount = inputTransaction.strip().split(' ')
+    amount = float(amount)
+    if key == ACTION_ADD_ACCOUNT:
+        return accounts.addAccount(name, amount)
+    elif key == ACTION_DEPOSIT_MONEY:
+        return accounts.depositMoney(name, amount)
+    elif key == ACTION_WITHDRAW_MONEY:
+        return accounts.withdrawMoney(name, amount)
+    else:
+        return 'Invalid'
 
 if __name__ == "__main__":
     sampleAccounts = Accounts()
     for line in sys.stdin:
-        print sampleAccounts.executeTransaction(line)
+        print executeTransaction(sampleAccounts, line)
