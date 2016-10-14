84ecaf3cfb061b08b749d85912d6533271447f46
CHAU HOANG THIEN AN <chta.fans@gmail.com>
2016-10-15 01:06:36 +0700

    clean up test

--- week0003/mattfanz/php/src/index.php
+++ week0003/mattfanz/php/src/index.php
@@ -2,16 +2,12 @@
 require_once('autoload.php');
 use FinTech\AccountManager;
 
-$handleIn = fopen("php://stdin", "r");
-$handleOut = fopen('php://stdout', 'w');
-
 $accountManager = new AccountManager();
 
-while (1) {
-    fwrite($handleOut, "Enter the action: ");
-    fscanf($handleIn, "%s %s %s", $action, $name, $amount);
+while ($action = fgets(STDIN)) {
+    sscanf($action, "%s %s %s", $action, $name, $amount);
     if ($action == 'E') {
         exit;
     }
-    fwrite($handleOut, $accountManager->makeAction($action, $name, $amount).PHP_EOL);
+    fwrite(STDOUT, $accountManager->makeAction($action, $name, $amount).PHP_EOL);
 }
