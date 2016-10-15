b9d280e4c194ee45c20660068e4db91c929f132d
Chanh Le <giaosudau@gmail.com>
2016-10-15 22:33:55 +0700

    fix it

--- week0003/giaosudau/python/week0003.py
+++ week0003/giaosudau/python/week0003.py
@@ -1,19 +1,23 @@
 import sys
 
+ACTION_ADD_CLIENT = 'A'
+ACTION_CLIENT_DEPOSIT = 'D'
+ACTION_CLIENT_WITHDRAW = 'W'
+
 
 class Client():
-    def __init__(self):
-        pass
-
     def __init__(self, name, amount):
         self.name = name
         self.amount = amount
 
     def deposit(self, amount):
-        self.amount += amount
-        return True
+        if amount > 0:
+            self.amount += amount
+            return True
+        else:
+            return False
 
-    def with_draw(self, amount):
+    def withdraw(self, amount):
         if amount <= self.amount:
             self.amount -= amount
             return True
@@ -22,7 +26,26 @@ class Client():
 
 
 class Bank(object):
-    clients = {}
+    def __init__(self):
+        self.clients = {}
+
+    def process(self, input):
+        for line in input:
+            data = line.split(" ")
+            action = data[0]
+            name = data[1]
+            amount = float(data[2])
+            if amount > 0:
+                if action == ACTION_ADD_CLIENT:
+                    print self.add_client(name, amount)
+                elif action == ACTION_CLIENT_DEPOSIT:
+                    print self.deposit(name, amount)
+                elif action == ACTION_CLIENT_WITHDRAW:
+                    print self.withdraw(name, amount)
+                else:
+                    print 'Invalid'
+            else:
+                print 'Invalid'
 
     def add_client(self, name, amount):
         if name in self.clients:
@@ -32,21 +55,6 @@ class Bank(object):
             self.clients[name] = client
             return True
 
-    def process(self, input):
-        for line in input:
-            data = line.split(" ")
-            action = data[0]
-            name = data[1]
-            amount = int(data[2])
-            if action == 'A':
-                print self.add_client(name, amount)
-            elif action == 'D':
-                print self.deposit(name, amount)
-            elif action == 'W':
-                print self.with_draw(name, amount)
-            else:
-                print 'Invalid'
-
     def deposit(self, name, amount):
         if name in self.clients:
             client = self.clients[name]
@@ -54,10 +62,10 @@ class Bank(object):
         else:
             return False
 
-    def with_draw(self, name, amount):
+    def withdraw(self, name, amount):
         if name in self.clients:
             client = self.clients[name]
-            return client.with_draw(amount)
+            return client.withdraw(amount)
         else:
             return False
 
