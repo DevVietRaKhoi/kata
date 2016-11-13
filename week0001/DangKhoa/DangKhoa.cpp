f14f2a88f01d4a25a1520bf7f1ad3aca6b9a9927
dangkhoa1992 <dangledangkhoa@gmail>
2016-11-13 16:01:55 +0800

    Remove std

--- week0001/DangKhoa/DangKhoa.cpp
+++ week0001/DangKhoa/DangKhoa.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-using namespace std;
 
 int main() {
 	for (int i = 1; i <= 100; ++i) {
 		if (i % 3 == 0) 
-			cout << "Fizz" << endl;
+			std::cout << "Fizz" << endl;
 		if(i % 5 == 0)
-			cout << "Buzz" << endl;
+			std::cout << "Buzz" << endl;
 	}
 	return 0;
 }
