9013ea20369318b10a7c67ab444b24e0a840f3a7
dangkhoa1992 <dangledangkhoa@gmail>
2016-11-13 15:38:47 +0800

    Test

---
+++ week0001/DangKhoa/DangKhoa.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 100; ++i) {
+		if (i % 3 == 0) 
+			cout << "Fizz" << endl;
+		if(i % 5 == 0)
+			cout << "Buzz" << endl;
+	}
+	return 0;
+}
